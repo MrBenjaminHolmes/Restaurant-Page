f28f22ebea4191a26be2d4700ba13131ecddba04
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-08-28 12:56:02 +0100

    Hard Coded Menu

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import "./styles.css";
 import renderHomepage from "./pages/homePage.js";
 import renderFooter from "./pages/footer.js"
 
+const homeBtn = document.querySelectorAll("button")[0];
+
+
+
 function clearContent() {
     // const content = document.querySelector("#content");
     while(content.firstChild) { 
@@ -10,18 +14,12 @@ function clearContent() {
 }
 
 
-
-
-renderHomepage();
+//renderHomepage();
 renderFooter();
 
-
-// import burger from './images/burger.png';
-
-   
-// const image = document.createElement("img");
-// image.src = burger;
-   
-// document.body.appendChild(image);
+homeBtn.addEventListener("click",()=>{
+    clearContent()
+    renderHomepage();
+})
 
 
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,23 +1,34 @@
-export  default function renderHomepage(){
+import burgerImg from '../images/burger.png';
+import tacoImg from '../images/Taco.png';
+import chipsImg from '../images/chips.png';
+
+
+export default function renderHomepage(){
     const content = document.querySelector("#content");
     const home = `
-                <div class="info">
+        <div class="info">
             <div class="text">Taste the Speed, Savor the Flavor!</div>
-            <p>Quick Bites is your go-to spot for delicious, fast, and fresh meals. Located in the heart of the city, this cozy eatery specializes in serving up a variety of quick yet satisfying dishes</p>
+            <p>Quick Bites is your go-to spot for delicious, fast, and fresh meals. Located in the heart of the city, this cozy eatery specializes in serving up a variety of quick yet satisfying dishes.</p>
             <h2 id="promoText">PROMOS</h2>
             <div id="tacot">🌮 Taco Tuesday 🌮</div>
             <div class="promos">
-                
-                <img id="Taco" src="images/Taco.png" alt="Taco">
-                <img id="Chips" src="images/chips.png" alt="chips">
             </div>
             <button>Order Now</button>
         </div>
-        <img src="images/burger.png" alt="burger">
-                `
+    `;
 
-                content.innerHTML = home;
-            }
-            
+    content.innerHTML = home;
+    const promos = document.querySelector(".promos");
+    const tacoImage = document.createElement("img");
+    const chipsImage = document.createElement("img");
+    promos.appendChild(tacoImage);
+    tacoImage.setAttribute("class", "taco");
+    promos.appendChild(chipsImage);
+    tacoImage.src = tacoImg;
+    chipsImage.src = chipsImg;
+    
+    const burgerImage = document.createElement("img");
+    content.appendChild(burgerImage);
+    burgerImage.src = burgerImg;
 
-
+}
